d5ba87b3652b8ffb53b99ffbbdbb5653776c9062
bonnland <bonnland@ucar.edu>
2025-12-29 14:47:59 -0700

    Install dsetsearch plugin at runtime

--- ckan/config/finish_jobs.sh
+++ ckan/config/finish_jobs.sh
@@ -3,7 +3,10 @@
 ##################################
 # /srv/app/finish_jobs.sh
 ##################################
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/srv/app/.local/bin
 
 set -x
+
+date
 
 /usr/local/bin/ckan -c /srv/app/ckan.ini harvester run
