15d0b3c28ef9c1e7f244be0ec64471fc3cf6906a
bonnland <bonnland@ucar.edu>
2026-08-31 15:00:55 -0600

    Change config to sync with development

--- ckan/docker-entrypoint.d/02_setup_config.sh
+++ ckan/docker-entrypoint.d/02_setup_config.sh
@@ -23,7 +23,6 @@ if [[ $CKAN__PLUGINS == *"dsetsearch"* ]]; then
   ckan config-tool --edit ./ckan.ini ckan.redis.url="${CKAN_REDIS_URL}"
 
   ckan config-tool ./ckan.ini ckanext.dsetsearch.enable_search_format_ui=true
-  ckan config-tool ./ckan.ini ckanext.dsetsearch.enable_publisher_facet=false
   ckan config-tool ./ckan.ini ckanext_dsetsearch_banner_message='test'
 
 
@@ -51,6 +50,7 @@ if [[ $CKAN__PLUGINS == *"dsetsearch"* ]]; then
   ckan config-tool --edit ./ckan.ini error_email_from=donotreply@ucar.edu
 
   #  These settings are subject to change, depending on the deployment
+  ckan config-tool ./ckan.ini ckanext.dsetsearch.enable_publisher_facet=false
   ckan config-tool --edit ./ckan.ini ckan.site_title='DASH Search'
   ckan config-tool --edit ./ckan.ini ckan.site_description='NCAR data search and discovery'
 
